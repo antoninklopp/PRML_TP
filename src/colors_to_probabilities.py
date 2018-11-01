1357af6647b431dd974e100e14eb0240d1a056d1
Yoan <yoan.souty@grenoble-inp.org>
2018-11-01 22:31:35 +0100

    Optimisation of RGB_to_rg

--- src/colors_to_probabilities.py
+++ src/colors_to_probabilities.py
@@ -29,13 +29,15 @@ def RGB_to_rg(img):
     image in rg Chromaticity format of same shape as input image img
     """
     res = np.zeros(img.shape)
-    for ind_r in range(img.shape[0]):
-        for ind_c in range(img.shape[1]):
-            pix = img[ind_r, ind_c]
-            res[ind_r, ind_c, 0] =int(pix[0]) + int(pix[1]) + int(pix[2])
-            if (res[ind_r, ind_c, 0]!=0):
-                res[ind_r, ind_c, 1] = pix[0] / res[ind_r, ind_c, 0]
-                res[ind_r, ind_c, 2] = pix[1] / res[ind_r, ind_c, 0]
+    R_values = img[:, :, 0].astype(int)
+    G_values = img[:, :, 1].astype(int)
+    B_values = img[:, :, 2].astype(int)
+    res[:, :, 0] = R_values + G_values + B_values
+    # Slightly correction to avoid 0 division
+    inds_null_L = np.where(res[:, :, 0] == 0)
+    res[:, :, inds_null_L] = 1
+    res[:, :, 1] = R_values / res[:, :, 0]
+    res[:, :, 2] = G_values / res[:, :, 0]
     return res
 
 
