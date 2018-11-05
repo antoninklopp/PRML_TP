9b02a912bd214fac0664a75f8a87bdd5217f021e
Florent GESLIN <Florent.Geslin@ensimag.grenoble-inp.fr>
2018-11-05 20:34:23 +0100

    on trace la courbe precision/recall

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,7 @@
 import numpy as np
 import sklearn.metrics as met
+import matplotlib.pyplot as plt
+
 
 def get_recall(Y_true, Y_pred):
     """
@@ -11,19 +13,19 @@ def get_precision(Y_true, Y_pred):
     """
     Calcul la precision d'un modèle grace à une prediction
     """
-    return 0
+    return met.precision_score(Y_true, Y_pred, average="macro")
 
 def get_accuracy(Y_true, Y_pred):
     """
     calcul l'exactitude d'un modèle grace à une prediction
     """
-    return 0
+    return met.accuracy_score(Y_true, Y_pred, average="macro")
 
 def get_confusion_matrix(Y_true, Y_pred):
     """
     calcul la matrice de confusion d'un prédiction
     """
-    pass
+    return met.confusion_matrix(Y_true, Y_pred)
 
 def plot_roc(Y_true, Y_proba):
     """
@@ -37,3 +39,17 @@ def plot_roc(Y_true, Y_proba):
     plt.text(0.50, 0.10,"AUC = "+str(roc_auc))
     plt.show()
     
+
+
+def plot_presion_recall_cruve(Y_true, Y_proba):
+    """
+    trace la precision en fonction du rappel
+    :param Y_true: vecteur contenant les bonnes prediction
+    :param Y_proba: vecteur contenant les scores
+    :return: rien trace une courbe
+    """
+    precision, recall, a = met.precision_recall_curve(Y_true, Y_proba)
+    plt.plot(recall, precision)
+    plt.xlabel('recall')
+    plt.ylabel('precision')
+    plt.show()
