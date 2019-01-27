12d8c84afdfdf680598011497a9a977a09f8e331
Antonin <antonin.klopptosser@faro.com>
2019-01-27 13:40:17 +0100

    Fixed network

--- src/lab3/lab3.py
+++ src/lab3/lab3.py
@@ -20,7 +20,7 @@ def extract_mask(matrix, center, w, h):
     for i in range(w2):
         for j in range(h2):
             batch[i][j] = matrix[center[0] - w2 +i][center[1] - h2 +j]
-            batch[w2+i][h2+j] = matrix[center[0] + i][center[1] + j] 
+            batch[w2+i][h2+j] = matrix[center[0] + i][center[1] + j]
 
     batch = np.array(batch)
     batch = batch.flatten()
@@ -39,19 +39,19 @@ def extract_batch(matrix, center, w, h):
     for i in range(w2):
         for j in range(h2):
             for k in range(3):
-                batch[k][i][j] = matrix[center[0] - w2 + i+1][center[1] - h2 + j+1][k]
-                batch[k][w2+i][h2+j] = matrix[center[0] + i+1][center[1] + j+1][k]
-                batch[k][i][h2+j] = matrix[center[0] -w2 + i+1][center[1] + j+1][k]
-                batch[k][w2+i][j] = matrix[center[0] + i+1][center[1] -h2+ j+1][k]
+                batch[k][i][j] = matrix[center[0] - w2 + i+1][center[1] - h2 + j+1][k]/255.0
+                batch[k][w2+i][h2+j] = matrix[center[0] + i+1][center[1] + j+1][k]/255.0
+                batch[k][i][h2+j] = matrix[center[0] -w2 + i+1][center[1] + j+1][k]/255.0
+                batch[k][w2+i][j] = matrix[center[0] + i+1][center[1] -h2+ j+1][k]/255.0
     return batch
 
 ## https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3?fbclid=IwAR0RPbWXqKaZJGbeqS_keLAh6gb8nz92GQzxavn4flP22xRCxIznX77es_Q
 def VGG_16(input_size):
     model = Sequential()
     model.add(ZeroPadding2D((1,1),input_shape=(3,input_size,input_size)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(input_size, (3, 3), activation='relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(input_size, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
     model.add(ZeroPadding2D((1,1)))
@@ -85,18 +85,14 @@ def VGG_16(input_size):
     model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
     model.add(Flatten())
-    model.add(Dense(4096, activation='relu'))
-    # model.add(Dropout(0.5))
-    model.add(Dense(input_size * input_size * 2, activation='relu'))
-    # model.add(Dropout(0.5))
     model.add(Dense(input_size * input_size, activation='softmax'))
 
     return model
 
 SIZE = 32
 
-data = get_all_masks(5)
-X_train, y_train = [], []
+data = get_all_masks(50)
+X_train = []; y_train = []
 test_data = 0
 # On crée les jeux d'entrainements....
 for img_name, mask in data:
@@ -108,7 +104,6 @@ for img_name, mask in data:
             X_train.append(x)
             y_train.append(y)
             test_data += 1
-        break
 
 print("NUMBER OF TEST DATA", test_data)
 
@@ -121,10 +116,10 @@ model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-model.summary()
+# model.summary()
 
 history = model.fit(np.array(X_train),
-                    np.array(y_train), epochs = 5)
+                    np.array(y_train), epochs = 3)
 
 ## Test the model
 test_data = get_test_masks()[:2]
@@ -144,6 +139,7 @@ for img_name, mask in test_data:
     print("test batches", len(list_to_predict))
     prediction = model.predict(np.array(list_to_predict))
     print("prediction shape", prediction.shape)
+    print("image", img_name)
 
     img_reconstruct = np.zeros(mask.shape)
 
@@ -152,8 +148,9 @@ for img_name, mask in test_data:
             l = x * x_size + y
             for i in range(SIZE):
                 for j in range(SIZE):
-                    if prediction[l][i*SIZE + j] > 0.5:
-                        img_reconstruct[x*x_size + i][y * y_size + j] = 1
+                    if prediction[l][i*SIZE + j] > 0.1:
+                        print(prediction[l][i*SIZE + j])
+                        img_reconstruct[x*x_size + i][y * y_size + j] = 255
                     else:
                         img_reconstruct[x*x_size + i][y * y_size + j] = 0
 
