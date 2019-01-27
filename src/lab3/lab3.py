bd57b41e28c11687d74024e50c1eb53b030617d1
Antonin <antonin.klopptosser@faro.com>
2019-01-27 15:08:34 +0100

    Tried to change for a patch-patch classification. Not working

--- src/lab3/lab3.py
+++ src/lab3/lab3.py
@@ -22,14 +22,16 @@ def extract_mask(matrix, center, w, h):
             batch[i][j] = matrix[center[0] - w2 +i][center[1] - h2 +j]
             batch[w2+i][h2+j] = matrix[center[0] + i][center[1] + j]
 
-    batch = np.array(batch)
-    batch = batch.flatten()
+    if np.sum(np.array(batch)) > 10:
+        return 1
+    else:
+        return 0
 
     return batch
 
 def extract_batch(matrix, center, w, h):
     batch = [[[0 for i in range(w)] for j in range(h)] for u in range(3)]
-    batch = np.ndarray(shape=(3,w,h))
+    batch = np.ndarray(shape=(w,h))
     w2 = w//2
     h2 = h//2
 
@@ -38,65 +40,72 @@ def extract_batch(matrix, center, w, h):
 
     for i in range(w2):
         for j in range(h2):
-            for k in range(3):
-                batch[k][i][j] = matrix[center[0] - w2 + i+1][center[1] - h2 + j+1][k]/255.0
-                batch[k][w2+i][h2+j] = matrix[center[0] + i+1][center[1] + j+1][k]/255.0
-                batch[k][i][h2+j] = matrix[center[0] -w2 + i+1][center[1] + j+1][k]/255.0
-                batch[k][w2+i][j] = matrix[center[0] + i+1][center[1] -h2+ j+1][k]/255.0
+            batch[i][j] = matrix[center[0] - w2 + i+1][center[1] - h2 + j+1]/255.0
+            batch[w2+i][h2+j] = matrix[center[0] + i+1][center[1] + j+1]/255.0
+            batch[i][h2+j] = matrix[center[0] -w2 + i+1][center[1] + j+1]/255.0
+            batch[w2+i][j] = matrix[center[0] + i+1][center[1] -h2+ j+1]/255.0
     return batch
 
 ## https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3?fbclid=IwAR0RPbWXqKaZJGbeqS_keLAh6gb8nz92GQzxavn4flP22xRCxIznX77es_Q
 def VGG_16(input_size):
-    model = Sequential()
-    model.add(ZeroPadding2D((1,1),input_shape=(3,input_size,input_size)))
-    model.add(Conv2D(input_size, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(input_size, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
+    # model = Sequential()
+    # model.add(ZeroPadding2D((1,1),input_shape=(3,input_size,input_size)))
+    # model.add(Conv2D(input_size, (3, 3), activation='relu'))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(input_size, (3, 3), activation='relu'))
+    # model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(128, (3, 3), activation='relu'))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(128, (3, 3), activation='relu'))
+    # model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(256, (3, 3), activation='relu'))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(256, (3, 3), activation='relu'))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(256, (3, 3), activation='relu'))
+    # model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(512, (3, 3), activation='relu'))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(512, (3, 3), activation='relu'))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(512, (3, 3), activation='relu'))
+    # model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(512, (3, 3), activation='relu'))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(512, (3, 3), activation='relu'))
+    # model.add(ZeroPadding2D((1,1)))
+    # model.add(Conv2D(512, (3, 3), activation='relu'))
+    # model.add(MaxPooling2D((2,2), strides=(2,2), dim_ordering="th"))
 
-    model.add(Flatten())
-    model.add(Dense(input_size * input_size, activation='softmax'))
+    # model.add(Flatten())
+    # model.add(Dense(input_size * input_size, activation=tf.nn.relu))
+    # model.add(Dense(2, activation='softmax'))
+
+    model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(input_size, input_size)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(2, activation=tf.nn.softmax)
+    ])
 
     return model
 
-SIZE = 32
 
-data = get_all_masks(50)
+SIZE = 16
+
+data = get_all_masks(100)
 X_train = []; y_train = []
 test_data = 0
 # On crée les jeux d'entrainements....
 for img_name, mask in data:
-    img = cv2.imread(img_name)
+    img = cv2.imread(img_name, 0)
     for (roi, center, w, h, angle) in sliding_windows(mask, SIZE, SIZE):
         x = extract_batch(img, center, SIZE, SIZE)
         y = extract_mask(mask, center, SIZE, SIZE)
@@ -113,20 +122,25 @@ model = VGG_16(SIZE)
 print("created model")
 
 model.compile(optimizer=tf.train.AdamOptimizer(),
-              loss='binary_crossentropy',
+              loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 # model.summary()
+X_train = np.array(X_train)
+y_train = y_train
 
-history = model.fit(np.array(X_train),
-                    np.array(y_train), epochs = 3)
+print("x train shape", X_train.shape)
+print("y train shape", len(y_train))
+
+history = model.fit(X_train,
+                    y_train, epochs = 3)
 
 ## Test the model
 test_data = get_test_masks()[:2]
 for img_name, mask in test_data:
     list_good = []
     list_to_predict = []
-    img = cv2.imread(img_name)
+    img = cv2.imread(img_name, 0)
     x_size = mask.shape[0]//SIZE
     y_size = mask.shape[1]//SIZE
     for (roi, center, w, h, angle) in sliding_windows(mask, SIZE, SIZE):
@@ -141,18 +155,15 @@ for img_name, mask in test_data:
     print("prediction shape", prediction.shape)
     print("image", img_name)
 
-    img_reconstruct = np.zeros(mask.shape)
+    img_reconstruct = np.zeros(img.shape)
 
     for x in range(x_size):
         for y in range(y_size):
             l = x * x_size + y
-            for i in range(SIZE):
-                for j in range(SIZE):
-                    if prediction[l][i*SIZE + j] > 0.1:
-                        print(prediction[l][i*SIZE + j])
-                        img_reconstruct[x*x_size + i][y * y_size + j] = 255
-                    else:
-                        img_reconstruct[x*x_size + i][y * y_size + j] = 0
+            print(prediction[l])
+            if prediction[l][1] > 0.5:
+                img_reconstruct[x*SIZE:(x+1)*SIZE, y*SIZE:(y+1)*SIZE] = img[x*SIZE:(x+1)*SIZE, y*SIZE:(y+1)*SIZE]
 
-    cv2.imwrite("test_model.png", img_reconstruct)
+    print("saved image", img_name[0] + "test_model.png")
+    cv2.imwrite(img_name[0] + "test_model.png", img_reconstruct)
     
