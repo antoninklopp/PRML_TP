7d0d313de3cecc51e103a82b878923b51663815f
Florent <florent.geslin@grenoble-inp.org>
2019-01-30 15:19:29 +0100

    notebook in order to test several models

--- src/lab3/lab3.py
+++ src/lab3/lab3.py
@@ -1,9 +1,14 @@
 import tensorflow as tf
 import tensorflow.keras as keras
-from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Dropout
-from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
-from keras.optimizers import SGD
+try:
+    from keras.models import Sequential
+    from keras.layers.core import Flatten, Dense, Dropout
+    from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
+    from keras.optimizers import SGD
+except ImportError:
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, Flatten, Dense, Dropout
+    from tensorflow.keras.optimizers import SGD
 import cv2
 from src.lab1.lab1_challenge2 import *
 from src.lab1.info_image import *
@@ -21,17 +26,18 @@ def extract_mask(matrix, center, w, h):
         for j in range(h2):
             batch[i][j] = matrix[center[0] - w2 +i][center[1] - h2 +j]
             batch[w2+i][h2+j] = matrix[center[0] + i][center[1] + j]
+            batch[i][h2+j] = matrix[center[0] -w2 + i+1][center[1] + j+1]
+            batch[w2+i][j] = matrix[center[0] + i+1][center[1] -h2+ j+1]
 
-    if np.sum(np.array(batch)) > 10:
+    if np.sum(np.array(batch)) > 250:
         return 1
     else:
         return 0
 
-    return batch
 
 def extract_batch(matrix, center, w, h):
     batch = [[[0 for i in range(w)] for j in range(h)] for u in range(3)]
-    batch = np.ndarray(shape=(w,h))
+    batch = np.ndarray(shape=(w,h,3))
     w2 = w//2
     h2 = h//2
 
@@ -98,72 +104,73 @@ def VGG_16(input_size):
     return model
 
 
-SIZE = 16
+if __name__ == "__main__":
 
-data = get_all_masks(100)
-X_train = []; y_train = []
-test_data = 0
-# On crée les jeux d'entrainements....
-for img_name, mask in data:
-    img = cv2.imread(img_name, 0)
-    for (roi, center, w, h, angle) in sliding_windows(mask, SIZE, SIZE):
-        x = extract_batch(img, center, SIZE, SIZE)
-        y = extract_mask(mask, center, SIZE, SIZE)
-        if x is not None and y is not None:
-            X_train.append(x)
-            y_train.append(y)
-            test_data += 1
+    SIZE = 16
 
-print("NUMBER OF TEST DATA", test_data)
+    data = get_all_masks(100)
+    X_train = []; y_train = []
+    test_data = 0
+    # On crée les jeux d'entrainements....
+    for img_name, mask in data:
+        img = cv2.imread(img_name, 0)
+        for (roi, center, w, h, angle) in sliding_windows(mask, SIZE, SIZE):
+            x = extract_batch(img, center, SIZE, SIZE)
+            y = extract_mask(mask, center, SIZE, SIZE)
+            if x is not None and y is not None:
+                X_train.append(x)
+                y_train.append(y)
+                test_data += 1
 
-# On crée le modèle
-model = VGG_16(SIZE)
+    print("NUMBER OF TEST DATA", test_data)
 
-print("created model")
+    # On crée le modèle
+    model = VGG_16(SIZE)
 
-model.compile(optimizer=tf.train.AdamOptimizer(),
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+    print("created model")
 
-# model.summary()
-X_train = np.array(X_train)
-y_train = y_train
+    model.compile(optimizer=tf.train.AdamOptimizer(),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
 
-print("x train shape", X_train.shape)
-print("y train shape", len(y_train))
+    # model.summary()
+    X_train = np.array(X_train)
+    y_train = y_train
 
-history = model.fit(X_train,
-                    y_train, epochs = 3)
+    print("x train shape", X_train.shape)
+    print("y train shape", len(y_train))
 
-## Test the model
-test_data = get_test_masks()[:2]
-for img_name, mask in test_data:
-    list_good = []
-    list_to_predict = []
-    img = cv2.imread(img_name, 0)
-    x_size = mask.shape[0]//SIZE
-    y_size = mask.shape[1]//SIZE
-    for (roi, center, w, h, angle) in sliding_windows(mask, SIZE, SIZE):
-        d = extract_batch(img, center, SIZE, SIZE)
-        m = extract_mask(mask, center, SIZE, SIZE)
-        if d is not None and m is not None:
-            list_good.append(m)
-            list_to_predict.append(d)
+    history = model.fit(X_train,
+                        y_train, epochs = 3)
 
-    print("test batches", len(list_to_predict))
-    prediction = model.predict(np.array(list_to_predict))
-    print("prediction shape", prediction.shape)
-    print("image", img_name)
+    ## Test the model
+    test_data = get_test_masks()[:2]
+    for img_name, mask in test_data:
+        list_good = []
+        list_to_predict = []
+        img = cv2.imread(img_name, 0)
+        x_size = mask.shape[0]//SIZE
+        y_size = mask.shape[1]//SIZE
+        for (roi, center, w, h, angle) in sliding_windows(mask, SIZE, SIZE):
+            d = extract_batch(img, center, SIZE, SIZE)
+            m = extract_mask(mask, center, SIZE, SIZE)
+            if d is not None and m is not None:
+                list_good.append(m)
+                list_to_predict.append(d)
 
-    img_reconstruct = np.zeros(img.shape)
+        print("test batches", len(list_to_predict))
+        prediction = model.predict(np.array(list_to_predict))
+        print("prediction shape", prediction.shape)
+        print("image", img_name)
 
-    for x in range(x_size):
-        for y in range(y_size):
-            l = x * x_size + y
-            print(prediction[l])
-            if prediction[l][1] > 0.5:
-                img_reconstruct[x*SIZE:(x+1)*SIZE, y*SIZE:(y+1)*SIZE] = img[x*SIZE:(x+1)*SIZE, y*SIZE:(y+1)*SIZE]
+        img_reconstruct = np.zeros(img.shape)
 
-    print("saved image", img_name[0] + "test_model.png")
-    cv2.imwrite(img_name[0] + "test_model.png", img_reconstruct)
-    
+        for x in range(x_size):
+            for y in range(y_size):
+                l = x * x_size + y
+                print(prediction[l])
+                if prediction[l][1] > 0.5:
+                    img_reconstruct[x*SIZE:(x+1)*SIZE, y*SIZE:(y+1)*SIZE] = img[x*SIZE:(x+1)*SIZE, y*SIZE:(y+1)*SIZE]
+
+        print("saved image", img_name[0] + "test_model.png")
+        cv2.imwrite(img_name[0] + "test_model.png", img_reconstruct)
